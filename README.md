1e103aef71ea7a44e386caf5118e068ca3603637
Chris <nandachristanto@ubuntu.ubuntu-domain>
2019-04-05 12:15:49 +0700

    "changes to index.js file and README.md"

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 
 Project Javascript to experiment with different data types: Number
 
-```console.log(0);
-console.log(100);
-console.log(3.14);
+- String in Javascript ES5 and ES6 
+- Math operation using console.log command
 
-const age = 52;
 
-console.log(`His age is ${age}`);
-
-alert(`His age is ${age}`)
-```
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 console.log(0);
-console.log(100);
+console.log(10*10);
 console.log(3.14);
+console.log(3+5); 
+console.log(15/3);
+Math.floor(15/4);
 
-const age = 52;
+const amount = 30;
+var height = 165; 
 
-console.log(`His age is ${age}`);
+console.log(`That indomie is ${amount}'s years old`);
+console.log("Yao Ming is not " + height + "cm");
 
-alert(`His age is ${age}`);
+var namaDepan = "Ayu"
+var namaBelakang = "Sari"
+
+console.log("Nama saya" + " " + namaDepan + namaBelakang)
+
+// ini bisa ngak ya
+console.log(`Nama Saya ${namaDepan + ' ' + namaBelakang} `)
+
+// alert(`I ate ${amount} donuts`);
